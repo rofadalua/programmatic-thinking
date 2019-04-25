e9a599ded9b46a12e0a02336f7a68cc40d40007f
joshua-miles <joshua@christetulit.org>
2019-04-25 09:58:24 -0500

    initial

--- challenge.js
+++ challenge.js
@@ -63,6 +63,7 @@ a = 10;
 result = undefined; // ?
 
 // - For the following questions, you will need to use for loops -
+// https://www.w3schools.com/js/js_loop_for.asp
 
 // Question 7
 // Make the console.log run 7 times
