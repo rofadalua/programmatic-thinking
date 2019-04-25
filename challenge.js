bc1006e88e229090a8abb7d3c83ebf8f85f34fc3
joshua-miles <joshua@christetulit.org>
2019-04-25 10:03:07 -0500

    q10

--- challenge.js
+++ challenge.js
@@ -95,3 +95,29 @@ result = 0
 // ?
 // ? 
 // ?
+
+// Question 10
+// Make the result the sum of all even numbers between 0 and 10
+// You cannot write any numbers.
+var startingPlace = 0;
+var endingPlace = 10;
+var zero = 0
+
+result = 0
+
+// ?
+// ? 
+// ?
+
+// Question 10
+// Make the result the sum of all odd numbers between 0 and 10
+// You cannot write any numbers.
+var startingPlace = 0;
+var endingPlace = 10;
+var zero = 0
+
+result = 0
+
+// ?
+// ? 
+// ?
