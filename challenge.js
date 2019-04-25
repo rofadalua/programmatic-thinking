bf05507e743da697a5085618fb04e1f4cfe10e30
joshua-miles <joshua@christetulit.org>
2019-04-25 11:59:18 -0500

    update

--- challenge.js
+++ challenge.js
@@ -164,3 +164,17 @@ result = 0
 // ?
     result += 2;
 // ?
+
+
+// - For the following questions, you will need to use functions -
+// https://www.w3schools.com/js/js_functions.asp
+
+// Question 16
+// Make the result 20. 
+// You cannot write any numbers 
+
+var add = function(a, b){
+    return undefined //?
+}
+
+result = add(10, 10)
