6a331e6b7304114faafb3bdb76cb18d513127336
Theodore Kimana <kimana.theodore@gmail.com>
2019-04-25 12:07:17 -0500

    initial

--- challenge.js
+++ challenge.js
@@ -9,58 +9,54 @@ a = 10;
 
 b = 20;
 
-result = undefined // ?
-
+result = a + b; // ?
 
 // Question 2
 // Make the result be 20
 
 a = 10;
 
-b = undefined; // ?
+b = 2; // ?
 
-result = a * b 
+result = a * b;
 
 // Question 3
 // Make the result be 5
 
 a = 10;
 
-b = undefined; // ?
+b = 2; // ?
 
-result = a / b 
+result = a / b;
 
 // Question 4
 // Make the result be the average of a, b, c, d
 
 a = 10;
 
-b = 0; 
+b = 0;
 
 c = 10;
 
-d = 0
+d = 0;
 
-
-result = undefined // ?
-
+result = (a + b + c + d) / 4; // ?
 
 // Question 5
 // Make the result be the remainder from when a and b are divided
 
 a = 10;
 
-b = 3; 
+b = 3;
 
-result = undefined // ?
-
+result = 10 % 3; // ?
 
 // Question 6
 // Make the result a boolean of whether or not a is an even number
 
 a = 10;
 
-result = undefined; // ?
+result = a % 2 !== 0; // ?
 
 // - For the following questions, you will need to use for loops -
 // https://www.w3schools.com/js/js_loop_for.asp
@@ -68,45 +64,48 @@ result = undefined; // ?
 // Question 7
 // Make the console.log run 7 times
 
-// ?
-    console.log('I am logging 7 times') 
-// ?
-
+var step;
+for (step = 0; step < 7; step = step + 1) {
+  console.log("I am logging 7 times");
+}
 
 // Question 8
 // Make the result equal to 70.
 // You cannot write any numbers.
+result = 0;
+a = 10;
+for (result = 0; result < 70; result += a) {}
 
-result = 0
-a = 10
 // ?
-// ? 
-// ?
-
 
 // Question 9
 // Make the result the sum of all numbers between 0 and 10
 // You cannot write any numbers.
 var startingPlace = 0;
 var endingPlace = 10;
+var currentNumber;
 
-result = 0
+result = 0;
 
-// ?
-// ? 
-// ?
+for (
+  currentNumber = startingPlace;
+  currentNumber <= endingPlace;
+  currentNumber += 1
+) {
+  result += currentNumber;
+}
 
 // Question 10
 // Make the result the sum of all even numbers between 0 and 10
 // You cannot write any numbers.
 var startingPlace = 0;
 var endingPlace = 10;
-var zero = 0
+var zero = 0;
 
-result = 0
+result = 0;
 
 // ?
-// ? 
+// ?
 // ?
 
 // Question 11
@@ -114,12 +113,12 @@ result = 0
 // You cannot write any numbers.
 var startingPlace = 0;
 var endingPlace = 10;
-var zero = 0
+var zero = 0;
 
-result = 0
+result = 0;
 
 // ?
-// ? 
+// ?
 // ?
 
 // Question 12
@@ -127,55 +126,53 @@ result = 0
 // You cannot write any numbers.
 var startingPlace = 0;
 var endingPlace = 10;
-var three = 3
+var three = 3;
 
-result = 0
+result = 0;
 
 // ?
-// ? 
+// ?
 // ?
 
 // Question 13
 // Make the result 7;
 // You cannot reference "result" anywhere that it is not already referenced.
 
-result = 0
+result = 0;
 
 // ?
-    result += 1;
+result += 1;
 // ?
 
 // Question 14
 // Make the result 10;
 // You cannot reference "result" anywhere that it is not already referenced.
 
-result = 0
+result = 0;
 
 // ?
-    result += 1;
+result += 1;
 // ?
 
 // Question 15
 // Make the result 10;
 // You cannot reference "result" anywhere that it is not already referenced.
 
-result = 0
+result = 0;
 
 // ?
-    result += 2;
+result += 2;
 // ?
-
 
 // - For the following questions, you will need to use functions -
 // https://www.w3schools.com/js/js_functions.asp
 
 // Question 16
-// Make the result 20. 
-// You cannot write any numbers 
+// Make the result 20.
+// You cannot write any numbers
 
-var add = function(a, b){
-    return undefined //?
-}
+var add = function(a, b) {
+  return undefined; //?
+};
 
-result = add(10, 10)
-
+result = add(10, 10);
