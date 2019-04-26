c56fe56fb364ac7219c46caa5ef17ce4805b384b
joshua-miles <joshua@christetulit.org>
2019-04-26 11:12:13 -0500

    -

---
+++ challenges/challenge-26.js
@@ -0,0 +1,22 @@
+// Challenge 25
+// Find the profit (the revenue - the expense) for each year
+// Do can only use "-" once
+// Do not create a function called subtract
+
+var revenue_year_1 = 100
+var expenses_year_1 = 200
+
+var revenue_year_2 = 200
+var expenses_year_2 = 300
+
+var revenue_year_3 = 100
+var expenses_year_3 = 200
+
+var revenue_year_4 = 200
+var expenses_year_4 = 300
+
+var total_profit_1 = undefined
+var total_profit_2 = undefined
+var total_profit_3 = undefined
+var total_profit_4 = undefined
+
