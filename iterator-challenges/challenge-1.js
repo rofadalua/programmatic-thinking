ff8cf90cbabd70a467ed0caaba57ca47925cd36d
joshua-miles <joshua@christetulit.org>
2019-04-26 16:18:54 -0500

    -

---
+++ iterator-challenges/challenge-1.js
@@ -0,0 +1,8 @@
+// Challenge 1
+// Make the code below run once for every element in the array 
+// (it should run three times)
+
+array = [ 1, 2, 3]
+// ?
+    console.log('I should run 3 times') 
+// ?
