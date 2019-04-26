513e19911c4a228ef203d66ccc09abf023c396b1
Theodore Kimana <kimana.theodore@gmail.com>
2019-04-26 10:50:16 -0500

    done

--- challenges/challenge-23.js
+++ challenges/challenge-23.js
@@ -5,12 +5,27 @@
 
 var revenue_1 = 100
 var revenue_2 = 100
+
 var revenue_3 = 400
 var revenue_4 = 800
+
 
 
 var expense_1 = 50
 var expense_2 = 50
 var expense_3 = 200
 var expense_4 = 400
+
+
+
+var add = function(number_1, number_2){
+    return number_1 + number_2
+}
+revenue_3_4 =add(revenue_3,revenue_4)
+revenue_1_2 = add(revenue_1,revenue_2)
+total_revenue = add(revenue_1_2,revenue_3_4)
+expenses_3_4 =add(expense_3,expense_4)
+expenses_1_2 = add(expense_1,expense_2)
+total_expense = add(expenses_1_2,expenses_3_4)
+profit = (total_revenue - total_expense)
 
