887e14f540d03adafa7e140d81839190f6225d4d
Theodore Kimana <kimana.theodore@gmail.com>
2019-04-26 16:33:06 -0500

    done

--- challenges/challenge-30.js
+++ challenges/challenge-30.js
@@ -1,6 +1,17 @@
 // Challenge 30
-// Write a function that returns true if 
+// Write a function that returns true if
 // the argument passed to it is between than 5 and 20
 // Do not write an "if" statement
 
+var betweenFiveAndTwenty = function(number) {
+  return number > 5 && number < 20;
+};
+
+var isSevenBetweenFiveAndTwenty = betweenFiveAndTwenty(7);
+// ///////
+
+
+var betweenFiveAndTwenty = function(number) {
+  return number < 5 && number < 20;
+};
 var isSevenBetweenFiveAndTwenty = betweenFiveAndTwenty(7)
