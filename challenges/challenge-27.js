fdbae63d50faf1ce39b113ccfc7b14078a8b8ecd
joshua-miles <joshua@christetulit.org>
2019-04-26 12:28:55 -0500

    -

---
+++ challenges/challenge-27.js
@@ -0,0 +1,15 @@
+// Challenge 27
+// Finish the isEven function
+// threeIsEven should === false
+// twoIsEven should === true
+
+var isEven = function(number){
+    if(number % 2 == 0){
+        // ?
+    } else {
+        //?
+    }
+}
+
+var threeIsEven = isEven(3)
+var twoIsEven = isEven(2)
