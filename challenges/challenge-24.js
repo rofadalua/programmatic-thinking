52443e07c7d55847e9bde42ee0e9b5a52fbddeb7
joshua-miles <joshua@christetulit.org>
2019-04-26 10:47:09 -0500

    -

---
+++ challenges/challenge-24.js
@@ -0,0 +1,13 @@
+// Challenge 24
+// Find the average revenue for each year
+// You can only use "+" once
+// you can only use "/" once
+
+var revenue_year_1_1 = 100
+var revenue_year_1_2 = 200
+
+var revenue_year_2_1 = 200
+var revenue_year_2_2 = 300
+
+var total_revenue_year_1 = undefined
+var total_revenue_year_2 = undefined
