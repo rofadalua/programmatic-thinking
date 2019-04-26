2ee47751ba6c31fe7ab3e600650649a0b833b7d0
joshua-miles <joshua@christetulit.org>
2019-04-26 10:01:33 -0500

    -

---
+++ challenges/challenge-23.js
@@ -0,0 +1,16 @@
+// Challenge 23
+// Find the profit (the sum of all the revenue minus the sum of all the expenses)
+// Only use "+" once
+// Onlue use "-" once
+
+var revenue_1 = 100
+var revenue_2 = 100
+var revenue_3 = 400
+var revenue_4 = 800
+
+
+var expense_1 = 50
+var expense_2 = 50
+var expense_3 = 200
+var expense_4 = 400
+
