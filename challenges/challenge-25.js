85da99818e78af7ee12d0620d16ad2bb6fb8ca64
Theodore Kimana <kimana.theodore@gmail.com>
2019-04-26 13:38:34 -0500

    done

--- challenges/challenge-25.js
+++ challenges/challenge-25.js
@@ -16,8 +16,8 @@ var revenue_year_3_b = 200
 var revenue_year_4_a = 200
 var revenue_year_4_b = 300
 
-var total_revenue_year_1 = undefined
-var total_revenue_year_2 = undefined
-var total_revenue_year_3 = undefined
-var total_revenue_year_4 = undefined
+var total_revenue_year_1 = average_of_2_numbers(revenue_year_1_a,revenue_year_1_b)
+var total_revenue_year_2 = average_of_2_numbers(revenue_year_2_a,revenue_year_2_b)
+var total_revenue_year_3 = average_of_2_numbers(revenue_year_3_a,revenue_year_3_b)
+var total_revenue_year_4 = average_of_2_numbers(revenue_year_4_a,revenue_year_4_b)
 
--- challenges/challenge-26.js
+++ challenges/challenge-26.js
@@ -2,6 +2,10 @@
 // Find the profit (the revenue - the expenses) for each year
 // Do can only use "-" once
 // Do not create a function called subtract
+const reduce_numbers = function(number_1, number_2){
+    return number_1 - number_2
+}
+
 
 var revenue_year_1 = 100
 var expenses_year_1 = 200
@@ -15,8 +19,9 @@ var expenses_year_3 = 400
 var revenue_year_4 = 400
 var expenses_year_4 = 500
 
-var total_profit_1 = undefined
-var total_profit_2 = undefined
-var total_profit_3 = undefined
-var total_profit_4 = undefined
+
+var total_profit_1 = reduce_numbers(revenue_year_1,revenue_year_2)
+var total_profit_2 = reduce_numbers(revenue_year_3,revenue_year_4)
+var total_profit_3 = reduce_numbers(expenses_year_1,expenses_year_2)
+var total_profit_4 = reduce_numbers(expenses_year_3,expenses_year_4)
 
