27f07e6b00be97f816b23a54790dc060fbbbaf40
joshua-miles <joshua@christetulit.org>
2019-04-28 08:33:03 -0500

    -

---
+++ iterator-challenges/challenge-7.js
@@ -0,0 +1,12 @@
+// Challenge 7
+// Complete this function to return the profit, given an array of revenue and expenses
+// a) Add all the revenue
+// b) Add all the expenses
+// c) return the difference
+
+var profit = function(revenue, expenses){
+
+    return undefined // ?
+}
+
+var result = profit([ 10, 10, 10 ], [ 5, 5, 5 ])
