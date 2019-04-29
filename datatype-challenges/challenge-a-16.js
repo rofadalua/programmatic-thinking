58fa9090b7bf557890558f0eaa75c61d3ddbb835
joshua-miles <joshua@christetulit.org>
2019-04-29 15:27:10 -0500

    -

---
+++ datatype-challenges/challenge-a-16.js
@@ -0,0 +1,13 @@
+// Challenge 16
+// Set result equal to 1.
+// Do not write 1.
+
+class Obj { 
+    constructor(one){
+        this.bar = one
+    }
+}
+
+var obj = new Obj(1)
+
+var result = undefined // ?
