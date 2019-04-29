eecd83e8d76f7f1d3e05ed67e3f2c53732b916ba
joshua-miles <joshua@christetulit.org>
2019-04-29 15:10:43 -0500

    -

---
+++ datatype-challenges/challenge-2.js
@@ -0,0 +1,11 @@
+// Challenge 2
+// Set result equal to 1.
+// Do not write 1.
+
+var obj = {
+    foo: {
+        bar: 1
+    }
+}
+
+var result = undefined // ?
---
+++ datatype-challenges/challenge-3.js
@@ -0,0 +1,13 @@
+// Challenge 2
+// Set result equal to 1.
+// Do not write 1.
+
+var obj = {
+    foo: {
+        baz: {
+            bar: 1
+        }
+    }
+}
+
+var result = undefined // ?
