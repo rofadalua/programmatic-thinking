611667fca5f4aa8de4284417fdde1e9e4a6ec2b6
Theodore Kimana <kimana.theodore@gmail.com>
2019-04-29 08:52:04 -0500

    done

--- iterator-challenges/challenge-7.js
+++ iterator-challenges/challenge-7.js
@@ -1,9 +1,18 @@
 // Challenge 7
 // Complete this function to return the profit, given an array of revenue and expenses
 
-var profit = function(revenue, expenses){
+var add = function(numbers) {
+  total_numbers = 0;
+  numbers.forEach(function(number) {
+    total_numbers += number;
+  });
+  return total_numbers;
+};
 
-    return undefined // ?
-}
+var profit = function(revenue, expenses) {
+  total_expenses = add(expenses);
+  total_revenue = add(revenue);
+  return total_revenue - total_expenses;
+};
 
-var result = profit([ 10, 10, 10 ], [ 5, 5, 5 ])
+var result = profit([10, 10, 10], [5, 5, 5]);
