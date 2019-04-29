3c57db088417f05a21d45bf789f635c7fc86e2dd
joshua-miles <joshua@christetulit.org>
2019-04-29 08:59:01 -0500

    -

---
+++ iterator-challenges/challenge-10.js
@@ -0,0 +1,4 @@
+// Challenge 10
+// Write a function which takes in an array of numbers
+// and returns a new array with every number from the first array + 2
+// do not use forEach (use map instead!)
