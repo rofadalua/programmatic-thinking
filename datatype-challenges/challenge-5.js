ec134e4747eb3d1fa66dc349e7037ce0485ba2d0
joshua-miles <joshua@christetulit.org>
2019-04-29 15:12:24 -0500

    -

---
+++ datatype-challenges/challenge-5.js
@@ -0,0 +1,13 @@
+// Challenge 5
+// Write a function that returns the value of bar.
+// Do not write 1.
+
+var obj = {
+    foo: {
+        baz: {
+            bar: 1
+        }
+    }
+}
+
+var result = undefined // ?
