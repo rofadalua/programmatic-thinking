c3f9936873aafe34a237996f0d28b13f7bf8f21f
joshua-miles <joshua@christetulit.org>
2019-04-29 15:31:28 -0500

    -

---
+++ datatype-challenges/challenge-a-22.js
@@ -0,0 +1,19 @@
+// Challenge 21
+// Set result equal to 1.
+// Do not write 1.
+
+class Obj { 
+
+    state = {
+        one: 1
+    }
+
+    method(){
+        return undefined // ?
+    }
+
+}
+
+var obj = new Obj
+
+var result = obj.method() 
