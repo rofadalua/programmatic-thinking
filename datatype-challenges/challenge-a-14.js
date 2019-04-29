fa22cd015af2f0f54d35f4de3162fd07a240304b
joshua-miles <joshua@christetulit.org>
2019-04-29 15:25:26 -0500

    -

---
+++ datatype-challenges/challenge-a-14.js
@@ -0,0 +1,23 @@
+// Challenge 14
+// Write a function that returns the the total number of students.
+// Do not write 54.
+// You can only use "+" once
+
+var flation = {
+    houston:{ 
+        cohorts: [ 
+            {
+                numberOfStudents: 27,
+                name: 'Houston[0]'
+            }, 
+            {
+                numberOfStudents: 12,
+                name: 'Bash the Hash'
+            },
+            {
+                numberOfStudents: 15,
+                name: 'Houston Codeo'
+            } 
+        ]
+    }
+}
