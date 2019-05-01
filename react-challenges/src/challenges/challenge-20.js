73d95dbc25c89fae8a940837b38916de96898eb9
joshua-miles <joshua@christetulit.org>
2019-05-01 14:04:15 -0500

    -

---
+++ react-challenges/src/challenges/challenge-20.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+export default class Challenge extends React.Component {
+
+    state = { 
+        listItems = [
+            {
+                message: "Hello World"
+            }, 
+            { 
+                message: "Goodbye World", 
+            },
+            {
+                message: "Code re-use is fun"
+            }
+        ]
+    }
+
+    render(){
+        return (
+            <ul>
+                {/* 
+                // Render 3 list items:
+                //  one that reads "Hello World",
+                //  one that reads "Goodbye World"
+                //  one that reads  "Code re-use is fun"
+                // Only write "li" once 
+                // Do not write "Hello World", "Goodbye World", or "Code re-use is fun"
+                */}
+            </ul>
+        )
+    }
+
+}
