c17399ca472c1e7cda52dc6e48afb30a9a0c94ba
joshua-miles <joshua@christetulit.org>
2019-05-01 13:12:15 -0500

    -

---
+++ react-challenges/src/challenges/challenge-02.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default class Challenge extends React.Component {
+
+    constructor(){
+        this.content = "Hello World"
+    }
+
+    render(){
+        
+        return (
+            <h1>{ /* Put "Hello World" here. Do not write Hello World.*/ }</h1>
+        )
+    }
+
+}
---
+++ react-challenges/src/challenges/challenge-03.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default class Challenge extends React.Component {
+
+    content = "Hello World"
+
+    render(){
+        return (
+            <h1>{ /* Put "Hello World" here. Do not write Hello World.*/ }</h1>
+        )
+    }
+
+}
