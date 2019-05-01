0875aaed1c0f48dce7a039f5e72f628c4bdf1c1d
joshua-miles <joshua@christetulit.org>
2019-05-01 13:37:16 -0500

    -

---
+++ react-challenges/src/challenges/challenge-12.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default class Challenge extends React.Component {
+
+    state = {
+        content = "Hello World"
+    }
+
+    render(){
+        return <Header />
+    }
+
+}
+
+class Header extends React.Component {
+    render(){
+        // Put "Hello World" in this h1 tag.
+        // Do not write Hello World 
+        return <h1>{undefined}</h1> 
+    }
+}
