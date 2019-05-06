464090a0a5a57321e0787ff45fa6b27fda02abec
joshua-miles <joshua@christetulit.org>
2019-05-06 13:52:12 -0500

    -

---
+++ challenges/challenge-36.js
@@ -0,0 +1,13 @@
+// Challenge 36
+// Set result = 15
+// Only write inside the houston codeo object
+
+var numberOfHoustonCodeoStudents = function(years){
+    
+}
+
+const houstonCodeo = {
+    // ?
+}
+
+var result = numberOfHoustonCodeoStudents(houstonCodeo) 
