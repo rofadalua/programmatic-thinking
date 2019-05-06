9467c743c07062c56e70fe9eb758a7b1c95de114
joshua-miles <joshua@christetulit.org>
2019-05-06 08:46:33 -0500

    -

---
+++ datatype-challenges/challenge-a-24.js
@@ -0,0 +1,15 @@
+// Challenge 24
+// Set result equal to 1.
+// Do not write insdie the Obj class.
+
+class Obj { 
+
+    setState(object){
+        let result = object.foo
+    }
+
+}
+
+var obj = new Obj
+
+// ?
