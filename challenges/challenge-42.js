5e46a0149f9aae002f89c53fa3f56a68602dd75f
joshua-miles <joshua@christetulit.org>
2019-05-07 12:51:24 -0500

    -

---
+++ challenges/challenge-42.js
@@ -0,0 +1,24 @@
+// Question 42
+// Set the result = 'victory'
+// Edit ball however you would like
+// Do not write 'victory'
+// Do not write ball
+// Do not edit the line with -X-
+
+var ball = {
+    // ?
+    inside: 'victory'
+    // ?
+}
+
+var firstPin = function(ball){
+    return ball.left
+}
+
+var secondPin = function(ball){
+    return ball.right
+}
+
+var firstPlace = firstPin(ball) // -X-
+var secondPlace = secondPin(ball) // -X-
+var result = undefined // ?
---
+++ challenges/challenge-43.js
@@ -0,0 +1,19 @@
+// Question 42
+// Set the result = 'victory'
+// Edit ball however you would like
+// Do not write 'victory'
+// Do not write ball
+// Do not edit the line with -X-
+
+var ball = {
+    // ?
+    inside: 'victory'
+    // ?
+}
+
+var firstPin = function(ball){
+    return ball.left
+}
+
+var restingPlace = firstPin(ball) // -X-
+var result = undefined // ?
