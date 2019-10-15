b14ebd9cce653034bdc923bc51a43b774f42d79e
Joshua Miles <30753026+Joshua-Miles@users.noreply.github.com>
2019-10-15 17:43:24 -0500

    Update index.js

--- react-challenges/src/index.js
+++ react-challenges/src/index.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
+import Challenge from './challenges-challenge-01.js'
 
-
-const attemptChallenge = num => require(`./challenges/challenge-${num}.js`).default
-
-const Challenge = attemptChallenge('01')
 ReactDOM.render(<Challenge />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
